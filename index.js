ee3a3b81d8957ca1b5c96425ea094b838218f107
Risto Koverola <risto.koverola@eficode.com>
2022-06-17 13:40:58 +0300

    Separate router into own module

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
 const { MONGODB_URI, PORT } = require('./utils/config')
 const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blogs')
 
 logger.info('Connecting to MongoDB')
 mongoose.connect(MONGODB_URI)
@@ -17,24 +17,7 @@ mongoose.connect(MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
