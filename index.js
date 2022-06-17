faa52019552e365724005dd087a34728e6f83039
Risto Koverola <risto.koverola@eficode.com>
2022-06-17 10:12:54 +0300

    Separate blog model into own module

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 require('dotenv').config()
 
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
