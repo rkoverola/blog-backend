dd7d55e14886ec59da94c7fc48f16a443990d38e
Risto Koverola <risto.koverola@eficode.com>
2022-06-30 15:00:17 +0300

    Solve 5.17

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const { errorHandler, requestLogger, tokenExtractor, userExtractor } = require('./utils/middleware')
+const testingRouter = require('./controllers/testing')
 
 // NOTE: Do not log url because it contains password
 const destination = MONGODB_URI.includes('testBlogDB')
@@ -35,6 +36,10 @@ app.use(userExtractor)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if(process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(errorHandler)
 
